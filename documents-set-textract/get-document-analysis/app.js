52225d11d5c18a767dcf0b1a6c10815abd71de65
EC2 Default User <ec2-user@ip-172-31-72-106.ec2.internal>
2019-05-09 09:19:21 +0000

    Add logic to handle NextToken for result.

--- documents-set-textract/get-document-analysis/app.js
+++ documents-set-textract/get-document-analysis/app.js
@@ -4,15 +4,33 @@ const s3 = new AWS.S3();
 
 exports.lambdaHandler = async(event, context) => {
     console.log(JSON.stringify(event));
-    const params = {
+    let params = {
         JobId: event.JobId,
         /* required */
-        MaxResults: '1000'
+        MaxResults: '100'
     };
-    const result = await textract.getDocumentAnalysis(params).promise();
-    
-    if(result.JobStatus === "SUCCEEDED")
+    let acc = [];
+    await getDocumentAnalysis(params, acc);
+
+    console.log(acc);
+    if (acc[0].JobStatus === "SUCCEEDED") {
         event.iterator.continue = false;
-    console.log(result);
+        
+        let result = {};
+        result.DocumentMetadata = acc[0].DocumentMetadata;
+        const reducer = (accumulator, currentValue) => accumulator.concat(currentValue);
+        result.Blocks = acc.map(c => c.Blocks).reduce(reducer, []);
+        console.log(JSON.stringify(result));
+    }
     return event;
 };
+
+const getDocumentAnalysis = async(params, acc) => {
+    const result = await textract.getDocumentAnalysis(params).promise();
+    console.log(result);
+    acc.push(result);
+    if (result.NextToken) {
+        params.NextToken = result.NextToken;
+        return await getDocumentAnalysis(params, acc);
+    }
+}
