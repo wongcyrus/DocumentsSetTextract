141f160b3058da7769a2a91a7619f1272dc670ae
EC2 Default User <ec2-user@ip-172-31-46-235.ec2.internal>
2019-05-14 01:09:19 +0000

    Upgrade to nodejs 10 and udpated setup script for new cloud9.

--- documents-set-textract/setup.sh
+++ documents-set-textract/setup.sh
@@ -1,6 +1,7 @@
 SourceBucket=documentssettextractsource
-nvm install v8.10
-nvm alias default 8.10
+nvm install v10.15.3
+nvm alias default 10.15.3
+npm update -g
 aws s3 mb s3://$SourceBucket
 
 ./run_all_npm.sh
