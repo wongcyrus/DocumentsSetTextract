027fd5c090cb31bb6376c5b2573fb27b7eb0c382
EC2 Default User <ec2-user@ip-172-31-91-232.ec2.internal>
2020-08-21 06:46:02 +0000

    Refactor pdf-to-images with await and async.

--- pdf-to-images/app.js
+++ pdf-to-images/app.js
@@ -1,71 +1,82 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
-const fs = require('fs');
-const { promisify } = require('util');
-const pdf2img = require('./pdf2img');
+const fs = require("fs");
+const { promisify } = require("util");
+const pdf2img = require("./pdf2img");
 
 const readFile = promisify(fs.readFile);
 
-exports.lambdaHandler = async(event, context) => {
-    console.log(JSON.stringify(event));
-    const srcBucket = event.bucket;
-    const srcKey = event.key;
+exports.lambdaHandler = async (event, context) => {
+  console.log(JSON.stringify(event));
+  const srcBucket = event.bucket;
+  const srcKey = event.key;
 
-    const destKeyPrefix = srcKey.replace(/\s/g, '_').replace(".pdf", "");
-    const outputdir = "/tmp/" + destKeyPrefix;
-    fs.existsSync(outputdir) || fs.mkdirSync(outputdir, { recursive: true });
-    const filePath = "/tmp/" + srcKey.replace(/\s/g, '_');
-    await s3download(srcBucket, srcKey, filePath);
+  const destKeyPrefix = srcKey.replace(/\s/g, "_").replace(".pdf", "");
+  const outputdir = "/tmp/" + destKeyPrefix;
+  fs.existsSync(outputdir) || fs.mkdirSync(outputdir, { recursive: true });
+  const filePath = "/tmp/" + srcKey.replace(/\s/g, "_");
+  await s3download(srcBucket, srcKey, filePath);
 
-    const stats = fs.statSync(filePath);
-    console.log('File Size in Bytes:- ' + stats.size);
-    const results = await convert2images(filePath, outputdir);
-    console.log(results);
+  const stats = fs.statSync(filePath);
+  console.log("File Size in Bytes:- " + stats.size);
+  const results = await convert2images(filePath, outputdir);
+  console.log(results);
 
-    const s3Results = await Promise.all(results.message.map(async c => {
-        console.log(c);
-        let data = await readFile(c.path);
-        let key = c.path.replace("/tmp/", "");
-        return await s3.putObject({ Bucket: process.env['ImagesBucket'], Key: key, Body: data, ContentType: 'image/png' }).promise();
-    }));
-    console.log(s3Results);
-    const images = results.message.map(c => c.path.replace("/tmp/", ""));
+  const s3Results = await Promise.all(
+    results.map(async (c) => {
+      console.log(c);
+      let data = await readFile(c.path);
+      let key = c.path.replace("/tmp/", "");
+      return await s3
+        .putObject({
+          Bucket: process.env["ImagesBucket"],
+          Key: key,
+          Body: data,
+          ContentType: "image/png",
+        })
+        .promise();
+    })
+  );
+  console.log(s3Results);
+  const images = results.map((c) => c.path.replace("/tmp/", ""));
 
-    return { srcBucket, srcKey, imagePrefix: images[0].replace("1.png", ""), numberOfImages: images.length };
+  return {
+    srcBucket,
+    srcKey,
+    imagePrefix: images[0].replace("1.png", ""),
+    numberOfImages: images.length,
+  };
 };
 
 const s3download = (bucketName, keyName, localDest) => {
-    if (typeof localDest == 'undefined') {
-        localDest = keyName;
-    }
-    let params = {
-        Bucket: bucketName,
-        Key: keyName
-    };
-    let file = fs.createWriteStream(localDest);
+  if (typeof localDest == "undefined") {
+    localDest = keyName;
+  }
+  let params = {
+    Bucket: bucketName,
+    Key: keyName,
+  };
+  let file = fs.createWriteStream(localDest);
 
-    return new Promise((resolve, reject) => {
-        s3.getObject(params).createReadStream()
-            .on('end', () => {
-                return resolve();
-            })
-            .on('error', (error) => {
-                return reject(error);
-            }).pipe(file);
-    });
+  return new Promise((resolve, reject) => {
+    s3.getObject(params)
+      .createReadStream()
+      .on("end", () => {
+        return resolve();
+      })
+      .on("error", (error) => {
+        return reject(error);
+      })
+      .pipe(file);
+  });
 };
 
-const convert2images = (filePath, outputdir) => {
-    pdf2img.setOptions({
-        type: 'png', // png or jpg, default jpg
-        density: 600, // default 600
-        outputdir, // output folder, default null (if null given, then it will create folder name same as file name)
-        outputname: 'p', // output file name, dafault null (if null given, then it will create image name same as input name)
-    });
-    return new Promise((resolve, reject) => {
-        pdf2img.convert(filePath, (err, info) => {
-            if (err) reject(err);
-            else resolve(info);
-        });
-    });
+const convert2images = async (filePath, outputdir) => {
+  pdf2img.setOptions({
+    type: "png", // png or jpg, default jpg
+    density: 600, // default 600
+    outputdir, // output folder, default null (if null given, then it will create folder name same as file name)
+    outputname: "p", // output file name, dafault null (if null given, then it will create image name same as input name)
+  });
+  return await pdf2img.convert(filePath);
 };
--- pdf-to-images/pdf2img.js
+++ pdf-to-images/pdf2img.js
@@ -1,210 +1,198 @@
 "use strict";
 
-const fs = require('fs');
-const gm = require('gm').subClass({
-    imageMagick: true
+const fs = require("fs");
+const gm = require("gm").subClass({
+  imageMagick: true,
 });
-const gs = require('node-gs');
-const path = require('path');
-const async = require('async');
+const gs = require("node-gs");
+const path = require("path");
 
 let options = {
-    type: 'png',
-    size: 1024,
-    density: 600,
-    outputdir: null,
-    outputname: null,
-    page: null
+  type: "png",
+  size: 1024,
+  density: 600,
+  outputdir: null,
+  outputname: null,
+  page: null,
 };
 
-let Pdf2Img = function() {};
+let Pdf2Img = function () {};
 
-Pdf2Img.prototype.setOptions = opts => {
-    options.type = opts.type || options.type;
-    options.size = opts.size || options.size;
-    options.density = opts.density || options.density;
-    options.outputdir = opts.outputdir || options.outputdir;
-    options.outputname = opts.outputname || options.outputname;
-    options.page = opts.page || options.page;
+Pdf2Img.prototype.setOptions = (opts) => {
+  options.type = opts.type || options.type;
+  options.size = opts.size || options.size;
+  options.density = opts.density || options.density;
+  options.outputdir = opts.outputdir || options.outputdir;
+  options.outputname = opts.outputname || options.outputname;
+  options.page = opts.page || options.page;
 };
 
-Pdf2Img.prototype.convert = (input, callbackreturn) => {
-    // Make sure it has correct extension
-    if (path.extname(path.basename(input)) != '.pdf') {
-        return callbackreturn({
-            result: 'error',
-            message: 'Unsupported file type.'
-        });
-    }
+Pdf2Img.prototype.convert = async (input) => {
+  // Make sure it has correct extension
+  if (path.extname(path.basename(input)) != ".pdf") {
+    return {
+      result: "error",
+      message: "Unsupported file type.",
+    };
+  }
 
-    // Check if input file exists
-    if (!isFileExists(input)) {
-        return callbackreturn({
-            result: 'error',
-            message: 'Input file not found.'
-        });
-    }
+  // Check if input file exists
+  if (!isFileExists(input)) {
+    return {
+      result: "error",
+      message: "Input file not found.",
+    };
+  }
 
-    let stdout = [];
-    let output = path.basename(input, path.extname(path.basename(input)));
+  let output = path.basename(input, path.extname(path.basename(input)));
 
-    // Set output dir
-    if (options.outputdir) {
-        options.outputdir = options.outputdir + path.sep;
-    }
-    else {
-        options.outputdir = output + path.sep;
-    }
+  // Set output dir
+  if (options.outputdir) {
+    options.outputdir = options.outputdir + path.sep;
+  } else {
+    options.outputdir = output + path.sep;
+  }
 
-    // Create output dir if it doesn't exists
-    if (!isDirExists(options.outputdir)) {
-        fs.mkdirSync(options.outputdir, { recursive: true });
-    }
+  // Create output dir if it doesn't exists
+  if (!isDirExists(options.outputdir)) {
+    fs.mkdirSync(options.outputdir, { recursive: true });
+  }
 
-    // Set output name
-    if (options.outputname) {
-        options.outputname = options.outputname;
-    }
-    else {
-        options.outputname = output;
-    }
+  // Set output name
+  if (options.outputname) {
+    options.outputname = options.outputname;
+  } else {
+    options.outputname = output;
+  }
 
-    async.waterfall([
-        // Get pages count
-        callback => {
-            gm(input).identify("%p ", function(err, value) {
-                let pageCount = String(value).split(' ');
-                if (!pageCount.length) {
-                    callback({
-                        result: 'error',
-                        message: 'Invalid page number.'
-                    }, null);
-                }
-                else {
-                    // Convert selected page
-                    if (options.page !== null) {
-                        if (options.page < pageCount.length) {
-                            callback(null, [options.page]);
-                        }
-                        else {
-                            callback({
-                                result: 'error',
-                                message: 'Invalid page number.'
-                            }, null);
-                        }
-                    }
-                    else {
-                        callback(null, pageCount);
-                    }
-                }
+  const pageCount = await getPageCount(input);
+  console.log("pageCount:" + pageCount);
+  const pageToImages = await Promise.all(
+    [...Array(pageCount).keys()].map(async (page) => {
+      let inputStream = fs.createReadStream(input);
+      page = parseInt(page, 10) + 1;
+      let outputFile =
+        options.outputdir +
+        options.outputname +
+        "_" +
+        page +
+        "." +
+        options.type;
 
-            })
+      return await convertPdf2Img(inputStream, outputFile, page);
+    })
+  );
+  console.log(pageToImages);
+  return pageToImages;
+};
 
-        },
-
-
-        // Convert pdf file
-        (pages, callback) => {
-            // Use eachSeries to make sure that conversion has done page by page
-            async.eachSeries(pages, (page, callbackmap) => {
-                let inputStream = fs.createReadStream(input);
-                page = parseInt(page) + 1
-                let outputFile = options.outputdir + options.outputname + '_' + page + '.' + options.type;
-
-                convertPdf2Img(inputStream, outputFile, page, (error, result) => {
-                    if (error) {
-                        return callbackmap(error);
-                    }
-
-                    stdout.push(result);
-                    return callbackmap(error, result);
-                });
-            }, (e) => {
-                if (e) callback(e);
-
-                return callback(null, {
-                    result: 'success',
-                    message: stdout
-                });
-            });
+const getPageCount = async (input) => {
+  return new Promise((resolve, reject) => {
+    try {
+      gm(input).identify("%p ", (err, value) => {
+        if (err) {
+          reject(err);
         }
-    ], callbackreturn);
+        console.log("gm identify ->" + value);
+        let pageCount = String(value).split(" ");
+        if (!pageCount.length) {
+          reject({
+            result: "error",
+            message: "Invalid page number.",
+          });
+        } else {
+          // Convert selected page
+          if (options.page !== null) {
+            if (options.page < pageCount.length) {
+              resolve(options.page);
+            } else {
+              reject({
+                result: "error",
+                message: "Invalid page number.",
+              });
+            }
+          } else {
+            console.log("return pageCount.length");
+            resolve(pageCount.length);
+          }
+        }
+      });
+    } catch (ex) {
+      reject(ex);
+    }
+  });
 };
-
-const convertPdf2Img = (input, output, page, callback) => {
+const convertPdf2Img = async (input, output, page) =>
+  new Promise((resolve, reject) => {
     let filepath;
     if (input.path) {
-        filepath = input.path;
-    }
-    else {
-        return callback({
-            result: 'error',
-            message: 'Invalid input file path.'
-        }, null);
+      filepath = input.path;
+    } else {
+      return reject({
+        result: "error",
+        message: "Invalid input file path.",
+      });
     }
     console.log(`input:${filepath}, output:${output}, page: ${page}`);
 
     gs()
-        .batch()
-        .nopause()
-        .option('-r' + options.density)
-        // .option('-dDownScaleFactor=2')
-        .option('-dFirstPage=' + page)
-        .option('-dLastPage=' + page)
-        .executablePath('/opt/bin/gs')
-        .device('png16m')
-        .output(output)
-        .input(filepath)
-        .exec((err, stdout, stderr) => {
-            console.log(stdout.toString('utf8'));
-            if (stderr) console.log(stderr.toString('utf8'));
-            if (err) {
-                return callback({
-                    result: 'error',
-                    message: err
-                }, null);
-            }
-            try {
-                if (!(fs.statSync(output)['size'] / 1000)) {
-                    return callback({
-                        result: 'error',
-                        message: 'Zero sized output image detected.'
-                    }, null);
-                }
+      .batch()
+      .nopause()
+      .option("-r" + options.density)
+      // .option('-dDownScaleFactor=2')
+      .option("-dFirstPage=" + page)
+      .option("-dLastPage=" + page)
+      .executablePath("/opt/bin/gs")
+      .device("png16m")
+      .output(output)
+      .input(filepath)
+      .exec((err, stdout, stderr) => {
+        if (stdout) console.log(stdout.toString("utf8"));
+        if (stderr) console.log(stderr.toString("utf8"));
+        if (err) {
+          return reject({
+            result: "error",
+            message: err,
+          });
+        }
+        try {
+          if (!(fs.statSync(output)["size"] / 1000)) {
+            return reject({
+              result: "error",
+              message: "Zero sized output image detected.",
+            });
+          }
 
-                let results = {
-                    page: page,
-                    name: path.basename(output),
-                    size: fs.statSync(output)['size'] / 1000.0,
-                    path: output
-                };
+          let results = {
+            page: page,
+            name: path.basename(output),
+            size: fs.statSync(output)["size"] / 1000.0,
+            path: output,
+          };
 
-                return callback(null, results);
-            }
-            catch (e) {
-                return callback(e);
-            }
-        });
-};
+          return resolve(results);
+        } catch (e) {
+          return reject(e);
+        }
+      });
+  });
 
 // Check if directory is exists
-const isDirExists = path => {
-    try {
-        return fs.statSync(path).isDirectory();
-    }
-    catch (e) {
-        return false;
-    }
-}
+const isDirExists = (path) => {
+  try {
+    return fs.statSync(path).isDirectory();
+  } catch (e) {
+    return false;
+  }
+};
 
 // Check if file is exists
-const isFileExists = path => {
-    try {
-        return fs.statSync(path).isFile();
-    }
-    catch (e) {
-        return false;
-    }
-}
+const isFileExists = (path) => {
+  try {
+    return fs.statSync(path).isFile();
+  } catch (e) {
+    return false;
+  }
+};
 
-module.exports = new Pdf2Img;
+module.exports = new Pdf2Img();
